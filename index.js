b4bf1fa12a1d3090272084719912ac67955e7873
Cory Forsythe <cory@coryforsythe.com>
2018-08-27 09:31:51 -0400

    Initial version

---
+++ index.js
@@ -0,0 +1,92 @@
+
+//Load third party node_modules
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    log4js = require('log4js'),
+    jsonxml = require('jsontoxml'),
+    jsonyaml = require('json2yaml'),
+    fs = require('fs');
+
+//configure logging
+log4js.configure({
+  appenders: { file: { type: 'file', filename: 'ChuckYeager.log' },console: { type: 'console' } },
+  categories: { default: { appenders: ['file','console'], level: 'debug' } }
+});
+
+//The Log4JS Logger
+var logger = log4js.getLogger();
+
+//The express server
+var app = express();
+
+
+//Load the Models and create routes for app
+function loadModels(){
+  try{
+    var modelFolder = './Models/';
+    //Read each file in the Models dir
+    fs.readdir(modelFolder, (err, files) => {
+      files.forEach(file => {
+        try{
+        logger.debug("Loading model "+file);
+        var fileData=fs.readFileSync(modelFolder+'/'+file, 'utf8');
+        var fileJson=JSON.parse(fileData);
+        if(!fileJson.response)
+          throw "Model JSON must have a top-level property named response";
+        var response=fileJson.response;
+
+        //Setup properties for route
+        var code=response.code || 200;
+        var body=response.body || {};
+        var method=response.method || 'get';
+        var path=response.path || '/';
+
+        //Emit route to express dynamically
+        app[method](path,function(req,res){
+          var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+          logger.debug("["+ip+"] Responding to "+path);
+          res.type('application/json; charset=utf8');
+          res.status(code);
+          res.send(body);
+        });
+
+        logger.info("Routing [ "+method+" "+path+" ("+code+") ]");
+
+      }
+      catch(iex){
+        logger.error("Unable to load model "+file+": "+iex);
+      }
+      });
+
+    });
+  }
+  catch(ex){
+    logger.error("Error loading models: "+ex);
+  }
+}
+
+//Start the Express Server
+function listen(port){
+  try{
+    //Handle JSON Body
+    app.use(bodyParser.json())
+
+    //Support HTTP GET for hosting health checks
+    app.get('/',function(req,res){
+      return res.sendStatus(200);
+    });
+
+    //Listen
+    app.listen(port);
+    var msg='Service Is Alive on Port '+port;
+    logger.info(msg)
+  }
+  catch(ex){
+    logger.error("There was a fatal error in the service: "+ex);
+  }
+}
+
+
+//Start Service
+loadModels();
+listen(8081);
--- readme.md
+++ readme.md
@@ -1,2 +1,48 @@
 # Chuck Yeager
 Named after the first American pilot to break the 'Mach' barrier, this tool assist in the rapid development of 'Mock' APIs for use in integration testing
+
+## Installation
+
+Install this package from the NPM repository
+
+```
+
+npm install chuckyeager
+
+```
+
+## Usage
+
+### 1: Configure the models
+The express server will create routes and response handlers based upon the contents of the Models sub-directory in this project. To create mocks, simply place a JSON file in that directory that conforms to this structure:
+
+```
+{
+  "response":{
+    "method":"get",
+    "path":"/examples",
+    "statusCode":200,
+    "body":{
+      "examples":[
+        {"firstName":"John","lastname":"Doe"}
+      ]
+    }
+  }
+}
+
+```
+
+The following properties of the JSON object can be adjusted or removed (in which case defaults will be used):
+
+|**Property**|**Usage**|**Default Value**|
+|---|---|---|
+|method|Defines the HTTP Method for the Express Route|GET|
+|path|Defines the relative path for the route (should be unique across the directory)||
+|statusCode|The HTTP Code to send with the response|200|
+|body|JSON body to send with the response|{}|
+
+### 2: Start the Server
+
+```
+node index.js
+```
